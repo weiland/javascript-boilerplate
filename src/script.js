b0ed5551a4659351ff871bf1b3719fa09515c6a1
Pascal Weiland <pascal@seehin.de>
2014-10-12 11:43:25 +0200

    updated script pattern

--- src/script.js
+++ src/script.js
@@ -1,11 +1,19 @@
-(function (window, undefined) {
+(function ( root , factory ) {
+  'use strict';
+  // AMD, CommonJS
+  // Browser globals
+  root.App = factory();
 
+})( function() {
   'use strict';
 
   var exports = {};
+  var privateOtto = true;
+
+  exports.val = privateOtto;
 
   exports.init = function () {};
 
   return exports;
 
-})(window);
+} );
